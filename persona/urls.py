107c3b02e03dff705b2108e6676290bc5600f96d
victor abitu <victor.ch370@gmail.com>
2016-09-30 00:00:24 -0300

    commit aplicacion persona

--- persona/urls.py
+++ persona/urls.py
@@ -5,7 +5,12 @@ from . import views
 urlpatterns = [
 
     url(r'^inicio$', views.mostrar_inicio),
+    url(r'^index$', views.mostrar_index),
     url(r'^inspector$', views.mostrar_inspector),
     url(r'^profesional$', views.mostrar_profesional),
     url(r'^jefeinspector$', views.mostrar_jefe_inspector),
+    url(r'^propietario$', views.mostrar_propietario),
+    url(r'^visador$', views.mostrar_visador),
+    url(r'^visar$', views.mostrar_visar),
+
 ]
--- persona/views.py
+++ persona/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def mostrar_inicio(request):
-    return render(request, 'inicio/index.html', {})
+    return render(request, 'inicio/base.html', {})
+
+def mostrar_index(request):
+    return render(request, 'inicio/index.html')
 
 def mostrar_inspector(request):
     return render(request, 'inspector/inspector.html', {})
@@ -11,3 +14,12 @@ def mostrar_profesional(request):
 
 def mostrar_jefe_inspector(request):
     return render(request, 'jefe_inspector/jefe_inspector.html')
+
+def mostrar_propietario(request):
+    return render(request, 'propietario/propietario.html')
+
+def mostrar_visador(request):
+    return render(request, 'visador/visador.html')
+
+def mostrar_visar(request):
+    return render(request, 'visador/visar.html')
