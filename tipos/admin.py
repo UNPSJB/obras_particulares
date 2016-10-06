450dbfb63064aa1707b9359a36da30dbb01a4aeb
JulianGarciaOkk <juliangarcia.edi@gmail.com>
2016-10-06 18:42:29 -0300

    "Nuevos modelos: Documento, TipoObra y Categoria"

--- tipos/admin.py
+++ tipos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(TipoDocumento)
+
+admin.site.register(TipoObra)
+
+admin.site.register(Documento)
+
+admin.site.register(Categoria)
--- tipos/models.py
+++ tipos/models.py
@@ -11,4 +11,33 @@ class TipoDocumento(models.Model):
     fecha_baja = models.DateTimeField()
 
     def __str__(self):
-        return nombre
+        return self.nombre
+
+
+class Documento(models.Model):
+
+    identificador = models.IntegerField()
+    tipoDocumento = models.ForeignKey(TipoDocumento)
+
+    def __str__(self):
+        return self.tipoDocumento.nombre
+
+
+
+class Categoria(models.Model):
+
+    tipo = models.CharField(max_length=2)
+    descripcion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "%s -> %s" % (self.tipo, self.descripcion)
+
+
+class TipoObra(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=100)
+    categoria = models.ForeignKey(Categoria)
+
+    def __str__(self):
+        return self.nombre
