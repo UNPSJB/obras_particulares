66e0cd863bf35f67eda482d7f6af32aa03b2ba8f
victor abitu <victor.ch370@gmail.com>
2016-10-09 17:44:32 -0300

    Arregle un error, movi la clase Persona abajo porque no reconocia la relacion one to one de los roles

--- persona/models.py
+++ persona/models.py
@@ -3,23 +3,6 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class Persona(models.Model):
-    SEXOS = [ {'F', 'Femenino'}, {'M', 'Masculino'} ]
-    dni =  models.CharField(max_length = 8, unique = True)
-    apellido  = models.CharField(max_length = 50)
-    nombre = models.CharField(max_length = 50)
-    mail = models.CharField(max_length = 20)
-    cuil = models.CharField(max_length = 14)    #el ultimo numero va a ser 00..09
-    domicilio = models.CharField(max_length = 50)
-    telefono = models.CharField(max_length = 15)
-    profesional = models.OneToOneField(Profesional, blank=True)
-    propietario = models.OneToOneField(Propietario, blank=True)
-    Usuario = models.OneToOneField(Usuario, blank=True)
-
-
-def __str__(self):
-    return "{}, {}" .format(self.apellido, self.nombre)
-
 class Rol(models.Model):
 
     class Meta:
@@ -47,4 +30,21 @@ class Usuario(Rol):
 
     def login(self):
         print("Iniciando secion")
+
+class Persona(models.Model):
+    SEXOS = [ {'F', 'Femenino'}, {'M', 'Masculino'} ]
+    dni =  models.CharField(max_length = 8, unique = True)
+    apellido  = models.CharField(max_length = 50)
+    nombre = models.CharField(max_length = 50)
+    mail = models.CharField(max_length = 20)
+    cuil = models.CharField(max_length = 14)    #el ultimo numero va a ser 00..09
+    domicilio = models.CharField(max_length = 50)
+    telefono = models.CharField(max_length = 15)
+    profesional = models.OneToOneField(Profesional, blank=True)
+    propietario = models.OneToOneField(Propietario, blank=True)
+    Usuario = models.OneToOneField(Usuario, blank=True)
+
+
+    def __str__(self):
+        return "{}, {}" .format(self.apellido, self.nombre)
 
