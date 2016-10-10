4c1d064b5a99b92337f643d5c20f6a681ada927c
Luciano Perdomo <lucho_696@hotmail.com>
2016-10-10 14:57:37 -0300

    Form: Alta Persona

--- persona/admin.py
+++ persona/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Persona)
+class ALtaPersona(admin.ModelAdmin):
+    fieldsets = [
+        ('Datos Persona', {'fields':['dni','nombre','apellido']}),
+        ('Informacion general',{'fields':['mail','cuil','domicilio','telefono']}),
+    ]
+
+    search_fields = ['nombre','apellido','dni']
+    list_display = ('apellido','nombre','dni','mail','cuil','domicilio','telefono')
+
+
+admin.site.register(Persona,ALtaPersona)
 admin.site.register(Propietario)
 admin.site.register(Usuario)
-
+admin.site.register(Profesional)
