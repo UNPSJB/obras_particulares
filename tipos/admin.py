f8204a565f88869f0f0af78c8acfb0f39049e10a
Luciano Perdomo <lucho_696@hotmail.com>
2016-10-09 21:28:13 -0300

    Formulario Alta de TipoDocuemnto

--- tipos/admin.py
+++ tipos/admin.py
@@ -3,7 +3,19 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(TipoDocumento)
+
+class AltaTipoDocumento(admin.ModelAdmin):
+    fieldsets = [
+        ('Informacion general', {'fields':['nombre']}),
+        (None,  {'fields':['descripcion']}),
+        (None,  {'fields':['activo']}),
+        ('Informacion de Fechas',{'fields':['fecha_alta']}),
+        (None, {'fields':['fecha_baja']})
+    ]
+
+
+
+admin.site.register(TipoDocumento,AltaTipoDocumento)
 
 admin.site.register(TipoObra)
 
