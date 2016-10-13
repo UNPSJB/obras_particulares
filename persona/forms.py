62271c51c46a59f9e339456c6338deb82128f21c
Luciano Perdomo <lucho_696@hotmail.com>
2016-10-13 10:30:07 -0300

    class meta en forms persona y tipos. falta linkear

--- persona/forms.py
+++ persona/forms.py
@@ -7,3 +7,21 @@ class FormularioPersona(forms.ModelForm):
     class Meta:
         model = Persona
         fields = ('nombre', 'apellido', 'telefono', 'dni', 'domicilio', 'telefono', 'cuil')
+
+
+class FormularioProfesional(forms.ModelForm):
+
+    class Meta:
+        model = Profesional
+        fields = ('matricula', 'profesion', 'categoria')
+
+
+class FormularioPropietario(forms.ModelForm):
+
+    class Meta:
+        model = Persona
+
+class FormularioUsuaro(forms.ModelForm):
+
+    class Meta:
+        fields = ('nombre_de_usuario')
--- tipos/forms.py
+++ tipos/forms.py
@@ -7,3 +7,15 @@ class FormularioTipoDocumento(forms.ModelForm):
     class Meta:
         model = TipoDocumento
         fields = ('nombre', 'descripcion', 'activo', 'fecha_alta')
+
+class FormularioDocumento(forms.ModelForm):
+
+    class Meta:
+        model = Documento
+        fields = ('identificador','tipoDocumento')
+
+class FormularioTipoObra(forms.ModelForm):
+
+    class Meta:
+        model = TipoObra
+        fields = ('nombre','descripcion','categorias')
