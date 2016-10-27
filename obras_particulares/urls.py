1280edaedd65f03eac7ae4cae49e96faaf269044
JulianGarciaOkk <juliangarcia.edi@gmail.com>
2016-10-27 17:24:46 -0300

    Cambios urls

--- obras_particulares/urls.py
+++ obras_particulares/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
+    url(r'^login$', views.login, name='login'),
+    url(r'^logout$', views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
-    url(r'', include('persona.urls')),
-    url(r'', include('tipos.urls')),
+    url(r'^persona/', include('persona.urls')),
+    url(r'^tipos/', include('tipos.urls')),
 
 ]
---
+++ obras_particulares/views.py
@@ -0,0 +1,5 @@
+def login(request):
+    pass
+
+def logout(request):
+    pass
